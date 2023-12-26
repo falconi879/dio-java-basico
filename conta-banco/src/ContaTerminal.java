5a8801abd938a7a936a895cc3419489a781221b2
falconi879 <falconi879@gmail.com>
2023-12-26 10:27:36 -0300

    finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Integer numero = 1021;
+        String agencia = "067-8";
+        String nomeCliente = "Mario Andrade";
+        Double saldo = 237.48;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor digite o numero da conta: ");
+        numero = sc.nextInt();
+        System.out.println("Por favor digite o numero da agencia: ");
+        agencia = sc.next();
+        System.out.println();
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco -  "
+        + numero+ " sua agencia é: "+ agencia + " e seu saldo é: "+ saldo + ", Já estão disponivel para saque!");
+      
+        
+        
         
     }
 }
